832f5283df29c0e86f920a8572214565340211ac
Sam Meech-Ward <sam@meech-ward.com>
2016-11-07 09:15:18 -0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Sam Meech-Ward on 2016-11-07.
+//  Copyright © 2016 Sam Meech-Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Sam Meech-Ward on 2016-11-07.
+//  Copyright © 2016 Sam Meech-Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
